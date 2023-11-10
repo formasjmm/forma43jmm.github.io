ef7a09a7ae616f32e49a513dd4d04a21d229593e
formasjmm <139526355+formasjmm@users.noreply.github.com>
2023-11-09 21:37:43 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyCy_pK9mzw21qMmauMa7YGXVhm4SRn95Mg",
-  authDomain: "formasjmm-80a89.firebaseapp.com",
-  projectId: "formasjmm-80a89",
-  storageBucket: "formasjmm-80a89.appspot.com",
-  messagingSenderId: "311633629164",
-  appId: "1:311633629164:web:4fdefcdf299fbe04813ac9",
-  measurementId: "G-ZY38F9RN08"
+   apiKey: "AIzaSyCy_pK9mzw21qMmauMa7YGXVhm4SRn95Mg",
+    authDomain: "formasjmm-80a89.firebaseapp.com",
+    projectId: "formasjmm-80a89",
+    storageBucket: "formasjmm-80a89.appspot.com",
+    messagingSenderId: "311633629164",
+    appId: "1:311633629164:web:4fdefcdf299fbe04813ac9",
+    measurementId: "G-ZY38F9RN08"
     });
